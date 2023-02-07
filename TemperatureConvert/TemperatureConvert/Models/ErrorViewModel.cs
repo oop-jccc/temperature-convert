1c99272dfd7c7ca3f8e01ecdb256f93fe42cdba8
Damon German <damon.german@hyland.com>
2023-02-07 08:25:39 -0600

    initial commit

---
+++ TemperatureConvert/TemperatureConvert/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TemperatureConvert.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
